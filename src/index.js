0024cb3692d3f407ae0d85c6225d285915c3fe19
Jeffery <jeffmc825@gmail.com>
2021-12-01 10:08:55 -0600

    commit tree 4286a3cba89101113c7fdbfd75987ef287191f5a

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
 console.log('%c HI', 'color: firebrick')
+const init = () => {
+    fetch ("https://dog.ceo/api/breeds/image/random/4")
+    .then(response => response.json())
+    .then(json =>{
+        json.message.forEach(function(element){
+            const img = document.createElement('img');
+            img.src = element;
+            document.querySelector("#dog-image-container").append(img);            
+        })
+    })
+    fetch ("https://dog.ceo/api/breeds/list/all")
+    .then(response => response.json())
+    .then(json =>{
+        for(const element in json.message){
+            const li = document.createElement('li')
+            li.textContent = element
+            document.querySelector("#dog-breeds").append(li)
+            li.addEventListener('click', function (){
+                this.style.color = "#FF0000";
+            })
+        }
+    
+        
+    })
+
+}
+
+document.addEventListener('DOMContentLoaded', init);
